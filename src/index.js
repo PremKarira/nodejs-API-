21090b8b8dcabc84577d25c1134123f49f350edc
prem <karira46@gmail.com>
2022-06-19 03:05:35 +0530

    completed upto sql queries and status codes

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import ip from 'ip';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import Response from './domain/response.js'
+import HttpStatus from './controller/patient.controller.js';
+import logger from './util/logger.js'
+
+dotenv.config();
+const PORT=process.env.SERVER_PORT || 3000;
+const app=express();
+app.use(cors({origin: '*'}));
+app.use(express.json());
+
+app.get('/',(req, res) => {res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API, v1.0.0 - All Systems Good'))});
+app.listen(PORT , () => logger.info(`Server running on: ${ip.address()}:${PORT}`));
